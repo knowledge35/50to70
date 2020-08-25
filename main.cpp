6a37de5a7822d0eff19134afdfff3d2488487272
knowledge35 <ibomyakmc@hotmail.com>
2020-08-25 08:02:09 +0300

    example

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+int main()
+{//1 den 100 kadar olan tek sayilari bastirin
+	for(int a=50; a<70; a++){
+		if(a%2==0){
+			cout<<a<<endl;
+		}
+	}
+   getch();
+   return 0;
+}
